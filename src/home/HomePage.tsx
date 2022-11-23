cd2e59e7c139691e94a03158b4bec283cffe4d33
alperaktepee <alperaktepe98@gmail.com>
2022-11-23 14:35:37 +0300

    first commit

---
+++ src/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+
+        <h1 style={{textAlign:"center"}}>TypeScript Tutorial With React</h1>
+    </div>
+  )
+}
+
+export default HomePage
